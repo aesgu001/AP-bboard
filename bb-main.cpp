a53c5706fe0de6356750af277836a62e531d1070
Angelo Pahati <aesgu001@github.com>
2022-05-04 18:38:56 -0700

    Add login menu prompt

--- bb-main.cpp
+++ bb-main.cpp
@@ -1,8 +1,100 @@
 #include "bb-main.h"
 
+std::string readLine()
+{
+    std::string line;
+    std::getline(std::cin, line, '\n');
+
+    return line;
+}
+
+bool readInteger(int &stInt)
+{
+    std::string numStr;
+    while (numStr.empty())
+    {
+        numStr = readLine();
+    }
+    
+    for (char c : numStr)
+    {
+        if (!isdigit(c))
+        {
+            return false;
+        }
+    }
+
+    stInt = std::stoi(numStr);
+
+    return true;
+}
+
+int enterOption(int numOptions)
+{
+    int option = 0;
+
+    std::cout << "Enter option: ";
+    while (!readInteger(option) || option <= 0 || option > numOptions)
+    {
+        std::cout << "ERROR! Please enter a valid option: ";
+    }   
+    std::cout << "\n";
+
+    return option;
+}
+
+void runLogin(bool &exitCalled, std::vector<BbUser> &users, BbUser **currUser)
+{
+    int option = 0;
+    std::cout   << "LOGIN MENU\n"
+                << "(1) Login\n"
+                << "(2) Register\n"
+                << "(3) Exit\n\n";
+    option = enterOption(3);
+
+    if (option == 1)
+    {
+        // TODO: login
+        std::cout << "Login\n\n";
+    }
+    else if (option == 2)
+    {
+        // TODO: register
+        std::cout << "Register\n\n";
+    }
+    else // option == 3
+    {
+        exitCalled = true;
+    }
+}
+
+void runBboard()
+{
+    std::vector<BbUser> users;
+    BbUser* currUser = nullptr;
+
+    bool exitCalled = false;
+
+    std::cout << "Welcome to AP Bulletin Board!\n\n";
+
+    while (!exitCalled)
+    {
+        if (!currUser)
+        {
+            runLogin(exitCalled, users, &currUser);
+        }
+        else
+        {
+            // TODO: runMessage
+        }
+    }
+
+    std::cout << "Goodbye!\n";
+}
+
 int main()
 {
-    std::cout << "Welcome to AP Bulletin Board!\n";
+    runBboard();
 
     return 0;
 }
--- bb-main.h
+++ bb-main.h
@@ -1,3 +1,46 @@
 #include "bb-user.h"
+// #include "bb-user.cpp"
 
-#include <iostream> // std::cout
+#include <iostream> // std::cin/cout
+#include <vector>   // std::vector
+
+/*
+*   Reads a string line from standard input.
+*
+*   @return A string of zero or more words.
+*/
+std::string readLine();
+
+/*
+*   Stores an integer converted from string input.
+*
+*   @param stInt variable to store the integer value.
+*
+*   @return True if the function converts the string successfully.
+*/
+bool readInteger(int&);
+
+/*
+*   Reads a number option from standard input.
+*
+*   @param numOptions the number of valid options starting from 1.
+*
+*   @return A valid number option.
+*/
+int enterOption(int);
+
+/*
+*   Opens a menu prompt with the following options: Login, Register, and Exit.
+*
+*   @param exitCalled flag to exit the program.
+*
+*   @return None.
+*/
+void runLogin(bool&, std::vector<BbUser>&, BbUser**);
+
+/*
+*   Launches the bulletin board, enabling user login/registration and posting messages.
+*
+*   @return None.
+*/
+void runBboard();
