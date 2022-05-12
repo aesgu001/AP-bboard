0a13973ae6bd6d78a8af5c224542f9d4aec7aa55
Angelo Pahati <aesgu001@github.com>
2022-05-11 23:27:08 -0700

    Overload stream operators for user class

--- bb-user.cpp
+++ bb-user.cpp
@@ -2,6 +2,9 @@
 
 #include "bb-user.h"
 
+const std::string BEGIN = "<begin_BBUser>";  // BBUser begin string limiter.
+const std::string END = "<end_BBUser>";      // BBUser end string limiter.
+
 BBUser::BBUser(const std::string &username, const std::string &password) : _username(username),
     _password(password)
 {}
@@ -29,6 +32,67 @@ BBUser::~BBUser()
 const std::string &BBUser::username() const
 {
     return this->_username;
+}
+
+bool BBUser::read(std::istream &in)
+{
+    std::string begin, end;
+    if (!(in >> begin))
+    {
+        // End-of-file is still a successful read
+        if (in.eof())
+        {
+            return true;
+        }
+
+        return false;
+    }
+    else if (begin != BEGIN)
+    {
+        return false;
+    }
+    else if (!(in >> this->_username) || !(in >> this->_password))
+    {
+        return false;
+    }
+    else if (!(in >> end) || end != END)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+void BBUser::write(std::ostream &out) const
+{
+    out << BEGIN << '\n'
+        << this->_username << '\n'
+        << this->_password << '\n'
+        << END;
+}
+
+std::istream &operator>>(std::istream &in, BBUser &obj)
+{
+    if (!obj.read(in))
+    {
+        // Get buffer error state
+        std::istream::iostate state = in.rdstate();
+
+        // Set fail state to 1 and end-of-file state to 0
+        state |= std::istream::failbit;
+        state &= ~std::istream::eofbit;
+
+        // Modify state
+        in.clear(state);
+    }
+
+    return in;
+}
+
+std::ostream &operator<<(std::ostream &out, const BBUser &obj)
+{
+    obj.write(out);
+    return out;
 }
 
 bool BBUser::match(const std::string &username, const std::string &password) const
--- bb-user.h
+++ bb-user.h
@@ -1,13 +1,29 @@
 #pragma once
 // BBUser class interface
 
-#include <string>
+#include <iostream> // std::istream, std::ostream, std::string
 
 class BBUser
 {
 private:
     std::string _username;  // ID name of the user.
     std::string _password;  // Password for accessing the user.
+    /*
+    *   Reads BBUser data from input stream.
+    *
+    *   @param in the input stream.
+    * 
+    *   @return True if the data being read is in proper format.
+    */
+    bool read(std::istream&);
+    /*
+    *   Writes BBUser data to output stream.
+    *
+    *   @param out the output stream.
+    * 
+    *   @return None.
+    */
+    void write(std::ostream&) const;
 public:
     // BBUser parameterized constructor
     BBUser(const std::string&, const std::string&);
@@ -21,6 +37,10 @@ public:
     ~BBUser();
     // BBUser username accessor
     const std::string &username() const;
+    // BBUser overloaded istream operator
+    friend std::istream &operator>>(std::istream&, BBUser&);
+    // BBUser overloaded ostream operator
+    friend std::ostream &operator<<(std::ostream&, const BBUser&);
     /*
     *   Checks if the username and password parameters match those of the object that called
     *   this function.
